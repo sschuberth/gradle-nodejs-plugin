a3b5323de2bcbab297fc2613d935b9bc9ec9b7be
Sebastian Schuberth <sschuberth@gmail.com>
2019-01-17 12:47:12 +0100

    NodeJsExtension: Omit optional semicolons for consistency

--- src/main/groovy/de/solugo/gradle/nodejs/NodeJsExtension.groovy
+++ src/main/groovy/de/solugo/gradle/nodejs/NodeJsExtension.groovy
@@ -2,6 +2,6 @@ package de.solugo.gradle.nodejs
 
 class NodeJsExtension {
     String version = "10.8.0"
-    String scriptBasePath = "";
-    String rootPath = "";
+    String scriptBasePath = ""
+    String rootPath = ""
 }
